3d09f0f0b3afa88fe22d804d61d6d489e07eb0d7
Maria Semionova <mauricelemon@my.smccd.edu>
2019-08-22 22:27:54 +0300

    Collection getting from server now

--- src/js/reports.js
+++ src/js/reports.js
@@ -1,15 +1,22 @@
 var Report = Backbone.Model.extend({
-  urlRoot: "../api/reports",
+  urlRoot: "../api/reports_db"
 });
 
 var Reports = Backbone.Collection.extend({
-  Model: Report
+  Model: Report,
+  url: "../api/reports_db"
 });
 
 var ReportView = Backbone.View.extend({
   initialize: function() {
+    this.model.on("add", this.onReportAdd, this);
     this.model.on("change", this.render, this);
     this.model.on("remove", this.onReportRemove, this);
+  },
+
+  onReportAdd: function(report) {
+    var reportView = new ReportView({model: report});
+    this.$el.append(reportView.render().$el);
   },
 
   onReportRemove: function(report) {
@@ -39,6 +46,7 @@ var ReportView = Backbone.View.extend({
 var ReportsView = Backbone.View.extend({
   tagName: "ul",
   className: "reports-cards",
+
   render: function() {
     this.collection.each(function (report) {
       var reportView = new ReportView({model: report});
@@ -48,33 +56,45 @@ var ReportsView = Backbone.View.extend({
   }
 });
 
+// MANUALY CREATED COLLECTION
+// var reports = new Reports([
+//   new Report({
+//     category: 'lolo',
+//     byUsername: 'Infersoul',
+//     byFullName: 'Maria Semionova',
+//     email: 'maria@mail.com',
+//     date: new Date(new Date() - 150000).toLocaleString('en-GB'),
+//     id: 1
+//   }),
+//   new Report({
+//     category: 'Users Activity',
+//     byUsername: 'Aosoth',
+//     byFullName: 'Anastasia Cernova',
+//     email: 'anastasia@mail.com',
+//     date: new Date(new Date() - 100000).toLocaleString('en-GB'),
+//     id: 2
+//   }),
+//   new Report({
+//     category: 'Conversion Rate',
+//     byUsername: 'Aosoth',
+//     byFullName: 'Anastasia Cernova',
+//     email: 'anastasia@mail.com',
+//     date: new Date().toLocaleString('en-GB'),
+//     id: 3
+//   })
+// ]);
+//
+// var reportsView = new ReportsView({collection: reports});
+// $('#reports').html(reportsView.render().$el);
 
-var reports = new Reports([
-  new Report({
-    category: 'Overall',
-    byUsername: 'Infersoul',
-    byFullName: 'Maria Semionova',
-    email: 'maria@mail.com',
-    date: new Date(new Date() - 150000).toLocaleString('en-GB'),
-    id: 1
-  }),
-  new Report({
-    category: 'Users Activity',
-    byUsername: 'Aosoth',
-    byFullName: 'Anastasia Cernova',
-    email: 'anastasia@mail.com',
-    date: new Date(new Date() - 100000).toLocaleString('en-GB'),
-    id: 2
-  }),
-  new Report({
-    category: 'Conversion Rate',
-    byUsername: 'Aosoth',
-    byFullName: 'Anastasia Cernova',
-    email: 'anastasia@mail.com',
-    date: new Date().toLocaleString('en-GB'),
-    id: 3
-  }),
-]);
-
-var reportsView = new ReportsView({collection: reports});
-$('#reports').html(reportsView.render().$el);
+//COLLECTION FROM SERVER
+var reps = new Reports();
+reps.fetch({
+  success: function() {
+    var repsView = new ReportsView({collection: reps});
+    $('#reports').html(repsView.render().$el);
+  },
+  error: function (errorResponse) {
+    console.log(errorResponse);
+  }
+});
